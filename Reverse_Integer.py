3810b10a3d10cade08516e81c6b0eccd1a28c21c
Actcheng <40320523+Actcheng@users.noreply.github.com>
2018-07-04 21:54:46 +0900

    Update Reverse_Integer.py

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,6 +1,7 @@
 # Problem #7
 # Date completed: 2018/07/04
 
+# Solution 1:
 class Solution:
     def reverse(self, x):
         """
@@ -22,3 +23,24 @@ class Solution:
         if ans>(2**31-1) or ans<(-(2**31)):
             ans=0
         return ans
+
+# Solution 2
+class Solution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+
+        s = -1+2*(x>0)
+        y = x*s
+        ans = 0
+        while y>0:   
+            ans = ans*10+y%10
+            y = y//10            
+        ans=s*ans
+        if (ans < (2**31)-1) and (ans>-(2**31)):
+            return ans
+        else:
+            return 0
+ 
