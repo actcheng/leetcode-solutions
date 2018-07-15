c1949e273844bef9aeda0d24349865a57a013284
Actcheng <40320523+Actcheng@users.noreply.github.com>
2018-07-15 14:09:06 +0900

    Create 0441_Arranging_Coins.py

---
+++ 0441_Arranging_Coins.py
@@ -0,0 +1,13 @@
+# Problem 441
+# Date completed: 2018/07/15
+
+class Solution(object):
+    def arrangeCoins(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        k = max(int(round((n*2)**(0.5)))-2,0)
+        while n >= k*(k+1)/2:
+            k+=1
+        return k-1
