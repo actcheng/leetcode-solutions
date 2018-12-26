8afcd5271f411803978f610e4a2adfacc69a6912
Actcheng <40320523+Actcheng@users.noreply.github.com>
2018-12-26 23:03:44 +0900

    Update 0038_Count_and_Say.py

--- 0038_Count_and_Say.py
+++ 0038_Count_and_Say.py
@@ -1,3 +1,4 @@
+# 52 ms
 class Solution:
     def countAndSay(self, n):
         """
@@ -26,3 +27,26 @@ class Solution:
             seq[i+1]=temp
         return seq[n-1]
                 
+# 44 ms
+class Solution:
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        seq ='1'
+
+        for i in range(n-1):
+            temp =''
+            count = 0
+            num = seq[0]
+            for n in seq:
+                if n == num:
+                    count+= 1
+                else:
+                    temp += str(count) + num
+                    num   = n
+                    count = 1
+            temp += str(count)+num
+            seq   = temp
+        return seq
