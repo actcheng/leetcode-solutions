303a8e10d7d58c60588ea423439712ff295d42b2
CT Cheng (Anna) <40320523+actcheng@users.noreply.github.com>
2019-04-08 15:45:59 +0900

    Solution 2

--- 0001_Two_sum.py
+++ 0001_Two_sum.py
@@ -12,3 +12,13 @@ class Solution:
             for j in range(len(nums)-1):
                 if (nums[i]+nums[j]==target and i!=j):
                     return [i,j]
+
+                class Solution:
+
+# Date completed: 2019/04/08
+# 1200 ms
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    ans = [i for i in range(len(nums)) if target-nums[i] in nums] 
+    if len(ans) > 2:
+        ans.remove(nums.index(int(target/2)))
+    return  ans
