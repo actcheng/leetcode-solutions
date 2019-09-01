40dc3d5c4ae395556af1a1829e15d1a44e0f31f6
CT Cheng (Anna) <40320523+actcheng@users.noreply.github.com>
2019-09-02 01:06:59 +0900

    Create 0551_Student_Attendance_Record_I.py

---
+++ 0551_Student_Attendance_Record_I.py
@@ -0,0 +1,20 @@
+# Problem 977
+# Date completed: 2019/09/01
+
+# 28 ms
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        absent = False
+        len_s = len(s)
+        for i in range(len_s):
+            x = s[i]
+            if x == 'A':
+                if absent:
+                    return False
+                else:
+                    absent = True
+            elif x == 'L':
+                if i < len_s-2 and s[i+1:i+3] == 'LL':
+                    return False
+                
+        return True
