2c580255eb9ef67ff010f0b426547498ab943782
CT Cheng (Anna) <40320523+actcheng@users.noreply.github.com>
2019-09-06 00:07:12 +0900

    Create Path_Sum.py

---
+++ Explore/Binary Tree/Solve Problems Recursively/Path_Sum.py
@@ -0,0 +1,12 @@
+# Date: 2019/09/05
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int, prevSum = 0) -> bool:
+        if root:            
+            prevSum += root.val
+            if not (root.left or root.right):
+                return prevSum == sum
+            return ((root.left and self.hasPathSum(root.left, sum, prevSum)) or 
+                    (root.right and self.hasPathSum(root.right, sum, prevSum)))  
+        else:
+            return False
