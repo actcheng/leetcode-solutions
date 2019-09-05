fcf68928b02c81c65663479b55ca83e65ccf00b0
CT Cheng (Anna) <40320523+actcheng@users.noreply.github.com>
2019-09-05 15:50:37 +0900

    Create Maximum_Depth_of_Binary_Tree.py

---
+++ Explore/Binary Tree/Solve Problems Recursively/Maximum_Depth_of_Binary_Tree.py
@@ -0,0 +1,6 @@
+# Date: 2019/09/05
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root: return 0
+        return max(self.maxDepth(root.left),self.maxDepth(root.right)) +1
