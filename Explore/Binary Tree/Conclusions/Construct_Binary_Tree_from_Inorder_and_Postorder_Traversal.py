7d0b4ba7ca156775e2daf220ae8a18666e995a42
CT Cheng (Anna) <40320523+actcheng@users.noreply.github.com>
2019-09-06 10:59:15 +0900

    Create Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py

---
+++ Explore/Binary Tree/Conclusions/Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py
@@ -0,0 +1,28 @@
+# Date: 2019/09/06
+
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        if not inorder or not postorder:
+            return None
+
+        root = TreeNode(postorder.pop())
+        ind = inorder.index(root.val)
+        # Cannot swap the order: postorder is public -> need to be used to build right tree first!        
+        root.right = self.buildTree(inorder[ind+1:],postorder)
+        root.left = self.buildTree(inorder[:ind],postorder)
+        
+        return root
+
+# Can swap order but need more space and slower!  
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        if not inorder or not postorder:
+            return None
+        
+        root = TreeNode(postorder[-1])
+        ind = inorder.index(root.val)
+            
+        root.right = self.buildTree(inorder[ind+1:],postorder[ind:-1])
+        root.left = self.buildTree(inorder[:ind],postorder[:ind])
+        
+        return root
