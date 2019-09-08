2f327d79192c50a9c19687dd04f45a695e993219
CT Cheng (Anna) <40320523+actcheng@users.noreply.github.com>
2019-09-09 00:11:05 +0900

    Create 0098_Validate_Binary_Search_Tree.py

---
+++ Explore/Binary Search Tree/Introduction to BST/0098_Validate_Binary_Search_Tree.py
@@ -0,0 +1,31 @@
+# Problem 98
+# Date: 2019/09/04
+
+# 40 ms
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        if not root: return True
+        
+        def checkBST(root,minimum,maximum):
+            left = right = True
+            if root.left:         
+                # print('Left',root.val, minimum, root.left.val,  root.left.val >= root.val or (minimum and root.left.val<=minimum))
+                if root.left.val >= root.val or (minimum and root.left.val<=minimum): return False
+                left = checkBST(root.left, minimum, root.val)
+            if root.right:
+                # print('Right',root.val, maximum, root.right.val, root.right.val <= root.val or (maximum and root.right.val>=maximum))
+                if root.right.val <= root.val or (maximum and root.right.val>=maximum): return False
+                right = checkBST(root.right, root.val,maximum)
+
+            return left and right
+        
+        return checkBST(root,None,None)
+    
