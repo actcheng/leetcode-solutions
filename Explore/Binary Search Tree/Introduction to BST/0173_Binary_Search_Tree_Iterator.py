e7dcebccea56d35b8c2ff8958299f9ffa9220e88
CT Cheng (Anna) <40320523+actcheng@users.noreply.github.com>
2019-09-09 18:13:45 +0900

    Create 0173_Binary_Search_Tree_Iterator.py

---
+++ Explore/Binary Search Tree/Introduction to BST/0173_Binary_Search_Tree_Iterator.py
@@ -0,0 +1,42 @@
+# Problem 173
+# Date: 2019/09/09
+
+# 176 ms
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+        self.pointer = []
+        node = root
+        while node:            
+            self.pointer.append([node, False])
+            node = node.left
+        print([[n.val, ret] for [n,ret] in self.pointer])
+            
+    def next(self) -> int:
+        """
+        @return the next smallest number
+        """
+        if not self.pointer: return None
+        root, returned = self.pointer.pop()              
+        
+        if root.right:
+            self.pointer.append([root, True])
+            node = root.right
+            while node:
+                self.pointer.append([node, False])
+                node = node.left
+                
+        nextVal = root.val
+        while self.pointer and self.pointer[-1][1]:
+            self.pointer.pop()
+            
+        print([[n.val, ret] for [n,ret] in self.pointer])
+        return nextVal
+        
+
+    def hasNext(self) -> bool:
+        """
+        @return whether we have a next smallest number
+        """
+        return len(self.pointer) > 0
+        
