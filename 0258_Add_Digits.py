4ea4d845935f888c0a0b658d2d39fba6cc332bb9
CT Cheng (Anna) <40320523+actcheng@users.noreply.github.com>
2019-09-13 14:51:52 +0900

    Update 0258_Add_Digits.py

--- 0258_Add_Digits.py
+++ 0258_Add_Digits.py
@@ -7,3 +7,11 @@ class Solution:
         while num > 9:
             num = sum([int(x) for x in str(num)])
         return num
+
+# 32 ms
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0: return 0
+        if num% 9 == 0: return 9
+        return num % 9
+        
