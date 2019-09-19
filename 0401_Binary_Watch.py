9f71cb465891c5fe1aacf7e21e9688020b4cebf9
CT Cheng (Anna) <40320523+actcheng@users.noreply.github.com>
2019-09-19 23:27:10 +0900

    Update 0401_Binary_Watch.py

--- 0401_Binary_Watch.py
+++ 0401_Binary_Watch.py
@@ -16,4 +16,20 @@ class Solution:
             times = [[t[0]+i//60,t[1]+i%60,t[2]+[i]] for i in intervals for t in times if i not in t[2] and t[0]+i//60 < 12 and t[1]+i%60 <60]
 
         return list(set(["{}:{}".format(t[0],str(t[1]).zfill(2)) for t in times]))
+    
+# 32 ms solution to learn from
+class Solution:
+    def readBinaryWatch(self, num: int) -> List[str]:
+        ans = []
+        for i in range(1024):
+            if bin(i).count('1') == num and i%64 < 60 and i//64 < 12:
+                mn = str(i%64)
+                if len(mn) < 2:
+                    mn = '0'+ mn
+                
+                hr = (i//64)
+                
+                ans.append(str(hr)+":"+mn)
+                
+        return ans
         
