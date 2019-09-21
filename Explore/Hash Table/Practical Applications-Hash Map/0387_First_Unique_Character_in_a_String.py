fcdfc7670e1ba726efcc07b7462de473fdbadffd
CT Cheng (Anna) <40320523+actcheng@users.noreply.github.com>
2019-09-21 16:49:42 +0900

    Update 0387_First_Unique_Character_in_a_String.py

--- Explore/Hash Table/Practical Applications-Hash Map/0387_First_Unique_Character_in_a_String.py
+++ Explore/Hash Table/Practical Applications-Hash Map/0387_First_Unique_Character_in_a_String.py
@@ -11,3 +11,26 @@ class Solution:
             return -1
         else:
             return min(ind)
+
+        # Date completed: 2019/09/11
+
+# 232 ms
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        unique = {}
+        checked = []
+        for i in range(len(s)):
+            c = s[i]            
+            if c in unique.keys():
+                del unique[c]
+            elif c not in checked:
+                unique[c] = i
+                checked.append(c)
+            
+        keys = list(unique.keys())
+        
+        if keys:
+            return unique[keys[0]]
+        else:
+            return -1
