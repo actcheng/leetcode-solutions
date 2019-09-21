7a0fe759ac4199044e4d5beed03db4c8c41f1cca
CT Cheng (Anna) <40320523+actcheng@users.noreply.github.com>
2019-09-21 14:38:29 +0900

    Update and rename 0202_Happy_Number.py to Explore/Hash Table/Practical Applications-Hash Set/0202_Happy_Number.py

--- Explore/Hash Table/Practical Applications-Hash Set/0202_Happy_Number.py
+++ Explore/Hash Table/Practical Applications-Hash Set/0202_Happy_Number.py
@@ -1,6 +1,15 @@
 # Problem 202
 # Date completed: 2019/09/12
 
+# 44 ms Hash set
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        hashset = set()
+        while n not in hashset: 
+            hashset.add(n)
+            n=sum([int(i)**2 for i in str(n)])              
+        return n==1 
+
 # 44 ms
 class Solution:
     def isHappy(self, n: int) -> bool:
