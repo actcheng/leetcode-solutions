b84e33d803370d0ca2f93a626e88003fefa50057
CT Cheng (Anna) <40320523+actcheng@users.noreply.github.com>
2019-10-30 10:57:18 +0900

    Create 1232_Check_If_It_Is_a_Straight_Line.py

---
+++ 1232_Check_If_It_Is_a_Straight_Line.py
@@ -0,0 +1,13 @@
+# Problem 1232 
+# Date completed: 2019/10/30 
+
+# 72 ms (80%)
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        if len(coordinates) < 3: return True
+        if coordinates[0][0]==coordinates[1][0]: # Vertical
+            return all(coordinates[0][0] == coord[0] for coord in coordinates[1:])
+        else:
+            slope = (coordinates[0][1]-coordinates[1][1])/(coordinates[0][0]-coordinates[1][0])
+            intercept = (coordinates[0][0]*coordinates[1][1]-coordinates[1][0]*coordinates[0][1])/(coordinates[0][0]-coordinates[1][0])
+            return all((coordinates[0][1]-coord[1])/(coordinates[0][0]-coord[0]) == slope and (coordinates[0][0]*coord[1]-coord[0]*coordinates[0][1])/(coordinates[0][0]-coord[0]) == intercept for coord in coordinates[1:])
