d955ffd86d83c16422deb8d270c69652e65e2562
CT Cheng (Anna) <40320523+actcheng@users.noreply.github.com>
2019-11-05 23:22:07 +0900

    Update 0118_Pascals_Triangle.py

--- Explore/Array and String/Introduction to 2D Array/0118_Pascals_Triangle.py
+++ Explore/Array and String/Introduction to 2D Array/0118_Pascals_Triangle.py
@@ -32,3 +32,21 @@ class Solution:
             else:
                 triangle.append([1]+[triangle[i-1][j]+triangle[i-1][j+1] for j in range(i-1) if i >0] + [1])
         return triangle
+
+# 2019/11/05
+# Recursive
+# 32 ms (94%)
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows==0: return []
+        res = [[0]*(i+1) for i in range(numRows)]
+        res[0][0]=1
+        def helper(i,j):            
+            if res[i][j] == 0: 
+                if j == 0 or j == i or i==0: 
+                    res[i][j] = 1
+                else:
+                    res[i][j] = helper(i-1,j-1)+helper(i-1,j)
+            return res[i][j]
+        [helper(numRows-1,i) for i in range(numRows)]
+        return res
