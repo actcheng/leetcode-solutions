8a78cce17c8406bc42e898641c9ca9d304345a2d
CT Cheng (Anna) <40320523+actcheng@users.noreply.github.com>
2019-11-05 20:53:41 +0900

    Update 0344_Reverse_String.py

--- Explore/Array and String/Two-Pointer Technique/0344_Reverse_String.py
+++ Explore/Array and String/Two-Pointer Technique/0344_Reverse_String.py
@@ -13,3 +13,17 @@ class Solution:
             s[i] = s[-(i+1)]
             s[-(i+1)] = temp
         
+# Date completed: 2019/11/05
+# Recusive
+
+# 260 ms
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        def helper(left,right):
+            s[left], s[right] = s[right],s[left]
+            if left+1 < right-1: helper(left+1,right-1)        
+        if not s: return 
+        helper(0,len(s)-1)
