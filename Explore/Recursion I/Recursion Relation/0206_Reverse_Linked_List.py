73066b5ebc0d529c3ad786603c9acd1bfaae7d3a
CT Cheng (Anna) <40320523+actcheng@users.noreply.github.com>
2019-11-06 23:12:51 +0900

    Update 0206_Reverse_Linked_List.py

--- Explore/Recursion I/Recursion Relation/0206_Reverse_Linked_List.py
+++ Explore/Recursion I/Recursion Relation/0206_Reverse_Linked_List.py
@@ -16,4 +16,24 @@ class Solution:
         head.next.next = head
         head.next = None
        
-        return tail    
+        return tail 
+    
+    
+# 36 ms (96%)
+# Iterative
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head: return head
+        stack = [head]
+        while stack[-1].next:
+            stack.append(stack[-1].next)
+            
+        tail = stack[-1]
+        while stack:
+            node = stack.pop()
+            if stack:
+                node.next = stack[-1]
+            else:
+                node.next = None
+                
+        return tail
