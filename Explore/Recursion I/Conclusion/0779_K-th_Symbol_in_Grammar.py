d05a37fffd5a36a3e87b6ca7c1f3c739ed861f6f
CT Cheng (Anna) <40320523+actcheng@users.noreply.github.com>
2019-11-08 00:33:10 +0900

    Create 0779_K-th_Symbol_in_Grammar.py

---
+++ Explore/Recursion I/Conclusion/0779_K-th_Symbol_in_Grammar.py
@@ -0,0 +1,20 @@
+# Problem 779
+# Date completed: 2019/11/07
+
+# 20 ms (100%)
+class Solution:
+    def kthGrammar(self, N: int, K: int) -> int:
+        if N == 1: 
+            return '0'
+        elif N == 2:
+            return '0' if K==1 else '1'
+        else:
+            ind = 2**(N-3)
+            if K <= ind*2: 
+                prevK = K
+            elif K<=ind*3:
+                prevK = K-ind
+            else:
+                prevK = K-ind*3
+            # print('prevK',prevK)
+            return self.kthGrammar(N-1,prevK)
