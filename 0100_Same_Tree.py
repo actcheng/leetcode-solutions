c96075cd63fb4bb06a7ee844a5979bc0dd13b51d
CT Cheng (Anna) <40320523+actcheng@users.noreply.github.com>
2019-11-16 00:05:14 +0900

    Update 0100_Same_Tree.py

--- 0100_Same_Tree.py
+++ 0100_Same_Tree.py
@@ -16,3 +16,21 @@ class Solution:
         if p.val != q.val: return False
         
         return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+
+# 2019/11/15
+# Iterative
+# 28 ms (95%)
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q: return True
+
+        stack = [(p,q)]
+        while stack:
+            a, b = stack.pop()
+            if (a and not b) or (b and not a): return False
+            if a.val != b.val: return False
+            if a.left or b.left:  stack.append((a.left,b.left))
+            if a.right or b.right: stack.append((a.right,b.right))
+                
+        return True    
