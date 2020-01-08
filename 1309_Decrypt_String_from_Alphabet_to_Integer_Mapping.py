7fdc2eb203ff040b9a2491a9bf759c8cf1cd5740
CT Cheng (Anna) <40320523+actcheng@users.noreply.github.com>
2020-01-08 23:07:30 +0900

    Create 1309_Decrypt_String_from_Alphabet_to_Integer_Mapping.py

---
+++ 1309_Decrypt_String_from_Alphabet_to_Integer_Mapping.py
@@ -0,0 +1,16 @@
+# Problem 1309
+# Date completed: 2020/01/08 
+
+# 28 ms (72%)
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        res = []
+        i = len(s)-1
+        while i>=0:
+            if s[i]=='#':
+                res.append(chr(ord('a')+int(s[i-2:i])-1)    )            
+                i-=3
+            else:
+                res.append(chr(ord('a')+int(s[i])-1))
+                i-=1
+        return ''.join(res[::-1])
