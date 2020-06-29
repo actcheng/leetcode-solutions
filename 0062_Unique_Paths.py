1d80202cce767c679e44636a89f6097c1172a0c2
CT Cheng (Anna) <40320523+actcheng@users.noreply.github.com>
2020-06-29 16:26:37 +0900

    Update 0062_Unique_Paths.py

--- 0062_Unique_Paths.py
+++ 0062_Unique_Paths.py
@@ -19,3 +19,20 @@ class Solution:
             if loc[1]+1 < n: helper((loc[0]  , loc[1]+1), paths[loc])    
                 
         return paths[m-1,n-1]
+
+# 2020/06/29
+# 24 ms (94%), DP solution
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m == 1 or n == 1: return 1
+        
+        prev_row = [1 for i in range(m)]
+        this_row = [1 for i in range(m)]
+        
+        for i in range(1,n):
+            for j in range(1,m):
+                this_row[j] = prev_row[j] + this_row[j-1]
+            prev_row, this_row = this_row, [1 for i in range(m)]
+            
+        return prev_row[-1]
